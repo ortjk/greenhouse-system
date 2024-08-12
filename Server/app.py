9489d707599deb2f7f32ed0b6c1135c0758181e1
Thomas Porter <ortjkdev@gmail.com>
2024-08-12 12:05:38 -0230

    Initial commit

---
+++ Server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def hello_world():
+    error = None
+    if request.method == "POST":
+        pass
+    else:
+        pass
+    return render_template("base_template.html", active=True)
